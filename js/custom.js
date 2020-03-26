ef64afdba887066161f71e35a4c7659b7f3f7a05
Jairo Muñoz <webmonster.co@Webmonster.local>
2020-03-26 03:33:53 -0500

    second commit

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,49 @@
-function ajax_get(url, callback) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            console.log('responseText:' + xmlhttp.responseText);
-            try {
-                var data = JSON.parse(xmlhttp.responseText);
-            } catch(err) {
-                console.log(err.message + " in " + xmlhttp.responseText);
-                return;
-            }
-            callback(data);
+var request = new XMLHttpRequest();
+request.open('GET', 'api/gradiAuthors.json', true);
+
+request.onload = function() {
+  if (this.status >= 200 && this.status < 400) {
+    // Success!
+    
+    var data = JSON.parse(this.response);
+
+    var html = "<div class='container'>";
+
+        html += "<div class='main-logo'><img src='assets/img/logo-full.png' alt='Logo' width='400'></div>";
+   
+        for (var i=0; i < 24; i++) {
+
+            if ((i % 4) == 0) html += "<div class='columns'>";
+            
+            html += "<div class='column'>";
+            html += "<div class='item-box' style='background: url("+ data[i]["download_url"] + ");'>";
+
+            html += "<div class='item-id'>" + data[i]["id"] + "</div>";
+            html += "<div class='logo-white'><img src='assets/img/logo-white.png'  alt='Logo Blanco' width='100'></div>";
+            html += "<div class='item-author'>" + data[i]["author"] + "</div>";
+
+            html += "</div>";
+            html += "</div>";
+
+            if ((i % 4) == 3) html += '</div>';
+              
         }
-    };
- 
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-}
- 
-ajax_get('../api/gradiAuthors.json', function(data) {
-    document.getElementById("id").innerHTML = data["id"];
- 
-    var html = "<h2>" + data["id"] + "</h2>";
-    html += "<h3>" + data["author"] + "</h3>";
-    html += "<ul>";
-       for (var i=0; i < data.length; i++) {
-           html += '<li><a href="' + data[i]["id"] + '">' + data[i]["author"] + "</a></li>";
-       }
-    html += "</ul>";
-    document.getElementById("text").innerHTML = html;
-});
+        
+        html += "</div>";
+	  
+      document.getElementById('wm-grid').innerHTML = html;
+	  
+      console.log(data.length);
+      console.log(data);
+    
+  } else {
+    // We reached our target server, but it returned an error
+
+  }
+};
+
+request.onerror = function() {
+  // There was a connection error of some sort
+};
+
+request.send();
